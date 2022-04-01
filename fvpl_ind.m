75d493537012bad4192129864373f2951496d17f
Danilo Rudrigues <101611580+DaniloRudrigues@users.noreply.github.com>
2022-04-01 17:03:01 -0300

    Add files via upload

---
+++ fvpl_ind.m
@@ -0,0 +1,11 @@
+function fit = fvpl_ind(x)
+
+% ps = length(x);
+% fit = zeros(ps,1);
+% for ii = 1:ps
+%     f = analysisDriver('imex_param.in','responseIMEX.out',x(ii,:));
+%     fit(ii) = f;
+% end
+
+outfile = [num2str(labindex) 'responseIMEX.out'];
+fit = analysisDriver_ind(outfile,x);
